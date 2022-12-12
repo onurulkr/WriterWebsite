4cf27f8269786bde10ee79d0b0814fa7362c89d6
onurulkr <55154950+onurulkr@users.noreply.github.com>
2022-12-12 17:36:58 +0300

    building relationships, key and string length attributes, context class, web.config configuration and connection setting, migrations and database creation

--- EntityLayer/Concrete/Content.cs
+++ EntityLayer/Concrete/Content.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,18 @@ namespace EntityLayer.Concrete
 {
     public class Content
     {
+        [Key]
         public int ContentId { get; set; }
+
+        [StringLength(1000)]
         public string ContentValue { get; set; }
         public DateTime ContentDate { get; set; }
-        //ContentYazar
-        //ContentBaşlık
+
+        public int HeadingId { get; set; }
+        public virtual Heading Heading { get; set; }
+
+        //public int WriterId { get; set; }
+        //public virtual Writer Writer { get; set; }
     }
 }
 
