38a40c1f98bf37297ee8c30886feac7569ca9f90
onurulkr <55154950+onurulkr@users.noreply.github.com>
2022-12-14 17:35:41 +0300

    add migration process, abstract and interface components, repository class and methods, Irepository interface, generic interface, generic repository, business layer, categories listing

--- EntityLayer/Concrete/Content.cs
+++ EntityLayer/Concrete/Content.cs
@@ -19,8 +19,8 @@ namespace EntityLayer.Concrete
         public int HeadingId { get; set; }
         public virtual Heading Heading { get; set; }
 
-        //public int WriterId { get; set; }
-        //public virtual Writer Writer { get; set; }
+        public int? WriterId { get; set; }
+        public virtual Writer Writer { get; set; }
     }
 }
 
---
+++ YazarWebsite/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YazarWebsite.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        CategoryManager cm = new CategoryManager();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetCategoryList()
+        {
+            var categoryvalues = cm.GetAllBL();
+
+            return View(categoryvalues);
+        }
+    }
+}
