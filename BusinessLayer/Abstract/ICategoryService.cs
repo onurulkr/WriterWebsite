49b9abb1323d32767e035e68a5ef483589273381
onurulkr <55154950+onurulkr@users.noreply.github.com>
2022-12-21 17:51:42 +0300

    category process with sidebar, getbyid, category delete, category update

--- BusinessLayer/Abstract/ICategoryService.cs
+++ BusinessLayer/Abstract/ICategoryService.cs
@@ -11,5 +11,9 @@ namespace BusinessLayer.Abstract
     {
         List<Category> GetList();
         void CategoryAdd(Category category);
+        Category GetById(int id);
+        void CategoryDelete(Category category);
+        void CategoryUpdate(Category category);
     }
 }
+
