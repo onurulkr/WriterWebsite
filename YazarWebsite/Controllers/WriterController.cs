fde1d506032b82b833a5a280544558df6f798367
onurulkr <55154950+onurulkr@users.noreply.github.com>
2022-12-23 17:15:31 +0300

    writer transactions, list of writer profiles

---
+++ YazarWebsite/Controllers/WriterController.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YazarWebsite.Controllers
+{
+    public class WriterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterDal());
+
+        public ActionResult Index()
+        {
+            var writervalues = wm.GetList();
+
+            return View(writervalues);
+        }
+    }
+}
