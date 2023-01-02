14ba7b97b526e1db9eb84264a4aa04d1981f0b53
onurulkr <55154950+onurulkr@users.noreply.github.com>
2023-01-02 17:45:44 +0300

    use of dropdownlist in header operations, writers creating titles and category operations, header operations and edits, content by heading pages, content controller, listing content by title and details of header transactions

---
+++ BusinessLayer/Concrete/ContentManager.cs
@@ -0,0 +1,51 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ContentManager : IContentService
+    {
+        IContentDal _contentDal;
+
+        public ContentManager(IContentDal contentDal)
+        {
+            _contentDal = contentDal;
+        }
+
+        public void ContentAdd(Content content)
+        {
+            _contentDal.Insert(content);
+        }
+
+        public void ContentDelete(Content content)
+        {
+            _contentDal.Delete(content);
+        }
+
+        public void ContentUpdate(Content content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Content GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Content> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Content> GetListByHeadingId(int id)
+        {
+            return _contentDal.List(x => x.HeadingId == id);
+        }
+    }
+}
--- YazarWebsite/Controllers/HeadingController.cs
+++ YazarWebsite/Controllers/HeadingController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,12 +12,61 @@ namespace YazarWebsite.Controllers
     public class HeadingController : Controller
     {
         HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        WriterManager wm = new WriterManager(new EfWriterDal());
 
         public ActionResult Index()
         {
             var headingvalues = hm.GetList();
 
             return View(headingvalues);
+        }
+
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryId.ToString()
+                                                  }).ToList();
+
+            List<SelectListItem> valuewriter = (from x in wm.GetList()
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.WriterName + " " + x.WriterSurname,
+                                                    Value = x.WriterId.ToString()
+                                                }).ToList();
+            ViewBag.vlc = valuecategory;
+            ViewBag.vlw = valuewriter;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddHeading(Heading p)
+        {
+            p.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            hm.HeadingAdd(p);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult EditHeading(int id)
+        {
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryId.ToString()
+                                                  }).ToList();
+            ViewBag.vlc = valuecategory;
+
+            var HeadingValue = hm.GetById(id);
+
+            return View(HeadingValue);
+
         }
     }
 }
