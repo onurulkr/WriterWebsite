ca5587f3a106c861e179a56c879288aeb0be14cf
onurulkr <55154950+onurulkr@users.noreply.github.com>
2023-01-13 15:44:46 +0300

    writer message operations

--- BusinessLayer/Concrete/ContentManager.cs
+++ BusinessLayer/Concrete/ContentManager.cs
@@ -47,5 +47,10 @@ namespace BusinessLayer.Concrete
         {
             return _contentDal.List(x => x.HeadingId == id);
         }
+
+        public List<Content> GetListByWriter()
+        {
+            return _contentDal.List(x => x.WriterId == 4);
+        }
     }
 }
