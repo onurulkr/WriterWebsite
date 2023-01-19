62ac80f41a57822ee31176d9e540e5a20933851b
onurulkr <55154950+onurulkr@users.noreply.github.com>
2023-01-19 11:58:21 +0300

    showcase page update and searching operations

--- YazarWebsite/Controllers/ContentController.cs
+++ YazarWebsite/Controllers/ContentController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,14 @@ namespace YazarWebsite.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetAllContent(string p)
+        {
+            var values = cm.GetList(p);
+            //var values = c.Contents.ToList();
+
+            return View(values.ToList());
         }
 
         public ActionResult ContentByHeading(int id)
